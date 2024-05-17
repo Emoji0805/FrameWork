82be36616022dd2ade6d9639f70bd42ba21814ef
Emoji0805 <mamihery.rabiazamaholy@gmail.com>
2024-05-17 10:31:04 +0300

    Sprint1_2743

--- src/FrontController.java
+++ src/FrontController.java
@@ -13,35 +13,35 @@ import javax.servlet.http.*;
 public class FrontController extends HttpServlet {
 
     private List<String> controllerNames;
+    private List<Class<?>> Listecontroller;
+    private boolean isChecked = false;
 
-    public void init() throws ServletException {
-        String packageToScan = this.getInitParameter("package_name");
+    // public void init() throws ServletException {
+    //     String packageToScan = this.getInitParameter("package_name");
 
-        controllerNames = new ArrayList<>();
+    //     controllerNames = new ArrayList<>();
 
-        try {
-            // Récupérer le répertoire racine du package
-            String path = getClass().getClassLoader().getResource(packageToScan.replace('.', '/')).getPath();
-            String decodedPath = URLDecoder.decode(path, "UTF-8");
-            File packageDir = new File(decodedPath);
+    //     try {
+    //         String path = getClass().getClassLoader().getResource(packageToScan.replace('.', '/')).getPath();
+    //         String decodedPath = URLDecoder.decode(path, "UTF-8");
+    //         File packageDir = new File(decodedPath);
 
-            // Parcourir tous les fichiers dans le répertoire du package
-            File[] files = packageDir.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile() && file.getName().endsWith(".class")) {
-                        String className = packageToScan + "." + file.getName().replace(".class", "");
-                        Class<?> clazz = Class.forName(className);
-                        if (clazz.isAnnotationPresent(ControllerAnnotation.class)) {
-                            controllerNames.add(clazz.getSimpleName());
-                        }
-                    }
-                }
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    //         File[] files = packageDir.listFiles();
+    //         if (files != null) {
+    //             for (File file : files) {
+    //                 if (file.isFile() && file.getName().endsWith(".class")) {
+    //                     String className = packageToScan + "." + file.getName().replace(".class", "");
+    //                     Class<?> clazz = Class.forName(className);
+    //                     if (clazz.isAnnotationPresent(ControllerAnnotation.class)) {
+    //                         controllerNames.add(clazz.getSimpleName());
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     } catch (IOException | ClassNotFoundException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         processedRequest(req, res);
@@ -53,14 +53,50 @@ public class FrontController extends HttpServlet {
     }
 
     protected void processedRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        res.setContentType("text/plain");
+        StringBuffer url = req.getRequestURL();
         PrintWriter out = res.getWriter();
-        out.println("Tongasoa ato am FrontController");
-        out.println("Votre url : " + req.getRequestURL().toString());
-
-        out.println("Liste des contrôleurs : ");
-        for (String controllerName : controllerNames) {
-            out.println(controllerName);
+        out.println("Go london : "+url);
+        
+        if(!this.isChecked){
+          String packageScan=this.getInitParameter("package_name");
+          try{
+               this.Listecontroller=this.getListeControllers(packageScan);
+               this.isChecked=true;
+          }
+          catch (Exception e){
+                e.printStackTrace();
+          }
         }
+        /* Affichage des Controller */
+        for (Class<?> classs : Listecontroller) {
+            out.println(classs.getName());
+        }
+    }
+
+    boolean isController(Class<?> c) {
+        return c.isAnnotationPresent(ControllerAnnotation.class);
+    }
+    List<Class<?>> getListeControllers(String packageName) throws Exception {
+        List<Class<?>> res=new ArrayList<Class<?>>();
+        
+        //répertoire racine du package
+        String path = this.getClass().getClassLoader().getResource(packageName.replace('.', '/')).getPath();
+        String decodedPath = URLDecoder.decode(path, "UTF-8");
+        File packageDir = new File(decodedPath);
+
+        // parcourir tous les fichiers dans le répertoire du package
+        File[] files = packageDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".class")) {
+                    String className = packageName + "." + file.getName().replace(".class", "");
+                    Class<?> classe = Class.forName(className);
+                    if (isController(classe)) {
+                        res.add(classe);
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
