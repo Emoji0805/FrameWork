1b2063c1f8805f7b368d7623a42a4d7686034e0b
Emoji0805 <mamihery.rabiazamaholy@gmail.com>
2024-11-17 18:32:12 +0300

    Sprint13_2743
    Validation de formulaire

--- src/Util.java
+++ src/Util.java
@@ -3,6 +3,8 @@ package Utils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletException;
 import java.lang.reflect.*;
+import java.util.Map;
+import java.lang.Number;
 import annotation.*;
 import javax.servlet.http.Part;
 
@@ -12,6 +14,7 @@ public class Util {
             Class<Param> paramAnnotationClass, Class<ParamObject> paramObjectAnnotationClass) throws Exception{
         Parameter[] parameters = method.getParameters();
         Object[] parameterValues = new Object[parameters.length];
+        Map<String, String[]> params = request.getParameterMap();
 
         for (int i = 0; i < parameters.length; i++) {
             if (parameters[i].isAnnotationPresent(paramAnnotationClass)) {
@@ -34,12 +37,19 @@ public class Util {
             } else if (parameters[i].isAnnotationPresent(paramObjectAnnotationClass)) {
                 ParamObject paramObject = parameters[i].getAnnotation(paramObjectAnnotationClass);
                 String objName = paramObject.objName();
+
                 try {
                     Object paramObjectInstance = parameters[i].getType().getDeclaredConstructor().newInstance();
                     Field[] fields = parameters[i].getType().getDeclaredFields();
                     for (Field field : fields) {
                         String fieldName = field.getName();
                         String paramValue = request.getParameter(objName + "." + fieldName);
+                        System.out.println(paramValue); 
+                        System.out.println(objName + "." + fieldName);
+
+                        String key = objName + "." + fieldName;
+                        
+                        validateField(params, field, key);
                         field.setAccessible(true);
                         field.set(paramObjectInstance, convertParameterValue(paramValue, field.getType()));
                     }
@@ -53,13 +63,73 @@ public class Util {
                 String paramName = parameters[i].getName();
                 String paramValue = request.getParameter(paramName);
                 parameterValues[i] = convertParameterValue(paramValue,
-                        parameters[i].getType());
+                                                        parameters[i].getType());
             }
         }
         return parameterValues;
     }
 
+     public static void validateField(Map<String, String[]> params, Field field, String key) throws Exception {
+
+        if(field.isAnnotationPresent(Required.class)) {
+            if (params.get(key) == null || params.get(key)[0].isEmpty()) {
+                throw new Exception("The parameter " + key + " is required.");
+            }
+        }
+
+        if (field.isAnnotationPresent(Length.class)) {
+        if (params.get(key) != null) {
+            String value = params.get(key)[0].replace(" ", "");
+            Length length = field.getAnnotation(Length.class);
+
+            if (value.length() > length.value()) {
+                throw new Exception("Le parametre " + key + " ne doit pas depasser les " + length.value() + " caracteres.");
+            }
+        }
+    }
+        
+        if (field.isAnnotationPresent(Numeric.class)) {
+            if (params.get(key) != null) {
+                try {
+                    Double.parseDouble(params.get(key)[0]);
+                } catch (Exception e) {
+                    throw new Exception("The parameter " + key + " must be a number.");
+                }
+            }
+        }
+        if (field.isAnnotationPresent(Range.class)) {
+            if (params.get(key) != null) {
+                try {
+                    double value = Double.parseDouble(params.get(key)[0]);
+                    Range range = field.getAnnotation(Range.class);
+                    if (value < range.min() || value > range.max()) {
+                        System.out.println("Ambany na ambony");
+                        throw new Exception("The parameter " + key + " must be within the range [" + range.min() + ", " + range.max() + "].");
+                    }
+                } catch (NumberFormatException e) {
+                    throw new Exception("The parameter " + key + " must be a number.");
+                }
+        
+            }
+        }
+
+    }
     public static Object convertParameterValue(String value, Class<?> type) {
+
+        //  if (value == null || value.isEmpty()) { // Vérifiez aussi si la chaîne est vide
+        //     if (type.isPrimitive()) {
+        //         if (type == int.class) return 0; // Valeur par défaut pour les int manquants
+        //         if (type == boolean.class) return false;
+        //         if (type == long.class) return 0L;
+        //         if (type == double.class) return 0.0;
+        //         if (type == float.class) return 0.0f;
+        //         if (type == short.class) return (short) 0;
+        //         if (type == byte.class) return (byte) 0;
+        //         if (type == char.class) return '\u0000';
+        //     }
+        //     return null;
+        // }
+
         if (type == String.class) {
             return value;
         } else if (type == int.class || type == Integer.class) {
